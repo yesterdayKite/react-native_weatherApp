c21d3096864f5786b3528465857cc0ca6b3eba90
yesterdaykite <yesterdaykite@yesterdaykites-MacBook-Pro.local>
2020-05-11 16:27:11 +0900

    before adding API

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert} from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class extends React.Component {
+  state = {
+    isLoading : true
+  };
+
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+     const {
+        coords : {latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.setState ({isLoading : false});
+    } catch(error) {
+      Alert.alert("can't find you", "so sad");
+    }
+  };
+
+  componentDidMount() {
+   this.getLocation();
+  }
+  render() {
+    const { isLoading} = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
